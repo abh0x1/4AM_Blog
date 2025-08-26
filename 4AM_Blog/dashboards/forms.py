956097783a4df579dde6f0c1a7c7168d80c9931e
Abhishek Kumar Verma <200225800+abh0x1@users.noreply.github.com>
2025-08-26 12:44:07 +0530

    4AM_Blog

---
+++ 4AM_Blog/dashboards/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from blogs.models import Blog, Category
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class BlogPostFform(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'category', 'featured_image',
+                  'short_description', 'blog_body', 'status', 'is_featured')
+
+
+class AddUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 'email', 'first_name', 'last_name',
+            'password1', 'password2', 'is_active', 'is_staff',
+            'is_superuser', 'groups', 'user_permissions'
+        ]
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username', 'email', 'first_name', 'last_name',
+            'is_active', 'is_staff',
+            'is_superuser', 'groups', 'user_permissions'
+        ]
